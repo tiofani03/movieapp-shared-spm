1374f15228c9f73bb4aaf853ecb0d06b76a5048d
Tio Fani <43690617+tiofani03@users.noreply.github.com>
2025-08-17 21:06:02 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Binary target dengan modul yang sama nama library
         .binaryTarget(
             name: "SharedMovieApp",
-            url: "https://github.com/tiofani03/movieapp-shared-spm/releases/download/0.0.2/Shared.xcframework.zip",
+            url: "https://github.com/tiofani03/movieapp-shared-spm/releases/download/0.0.2/Shared.xcframework.zip?raw=true",
             checksum: "8ee7aa153c932ad8355fe9e97bdf63456a583339fe6de1b31eafc1439366f1b3"
         )
     ]
