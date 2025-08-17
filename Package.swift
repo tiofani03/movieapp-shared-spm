044ce00b3f86b0b102ac05ab406645881b498462
Tio Fani <43690617+tiofani03@users.noreply.github.com>
2025-08-17 20:58:41 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,14 +7,16 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
+        // Library publik, modulnya sama dengan target binary
         .library(
             name: "SharedMovieApp",
-            targets: ["Shared"]
+            targets: ["SharedMovieApp"]
         )
     ],
     targets: [
+        // Binary target dengan modul yang sama nama library
         .binaryTarget(
-            name: "Shared",
+            name: "SharedMovieApp",
             url: "https://github.com/tiofani03/movieapp-shared-spm/releases/download/0.0.2/Shared.xcframework.zip",
             checksum: "8ee7aa153c932ad8355fe9e97bdf63456a583339fe6de1b31eafc1439366f1b3"
         )
