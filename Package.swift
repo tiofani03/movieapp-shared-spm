1ccbf0b9f2c158177c932bc83ec46e03b7acaa50
Tio Fani <43690617+tiofani03@users.noreply.github.com>
2025-08-17 20:24:19 +0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "SharedMovieApp",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SharedMovieApp",
+            targets: ["Shared"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Shared",
+            url: "https://github.com/tiofani03/movieapp/releases/download/0.0.2/Shared.xcframework.zip",
+            checksum: "8ee7aa153c932ad8355fe9e97bdf63456a583339fe6de1b31eafc1439366f1b3"
+        )
+    ]
+)
